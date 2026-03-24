fce7c240bf439680635f6e6db3d852be31b358fa
rubenvanderlugt <rubenvdl@hotmail.nl>
2026-03-24 09:23:44 +0100

    Update reserveer_st_antonisloop.py

--- reserveer_st_antonisloop.py
+++ reserveer_st_antonisloop.py
@@ -32,7 +32,7 @@ def wacht_tot_middernacht():
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
-print"nl_now"
+print(nl_now)
     # 0. Wacht to X tijd
     wacht_tot_middernacht()
 
